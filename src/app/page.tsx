d020f5770a9c609430dab309ed9e41a6b45b1268
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-12-03 13:54:13 +0530

    added Small Video Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className="py-2 px-8 flex gap-x-4 gap-y-14 flex-wrap justify-between items-start">
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
-      <VideoCard size="big" />
+      <VideoCard size="small" />
+      <VideoCard size="small" />
+      <VideoCard size="small" />
+      <VideoCard size="small" />
     </main>
   );
 }
--- src/components/VideoCard/SmallVideoCard.tsx
+++ src/components/VideoCard/SmallVideoCard.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import "./video-card.css";
+import { formatDate, formatTime, relativeTime } from "@/helpers/dateTime";
+
+const videoDetails = {
+  id: "ekPbZqPvCRA",
+  thumbnail:
+    "https://i.ytimg.com/vi/ekPbZqPvCRA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC7RDTSW1V2Sz3I4k19ahgs4LapfA",
+  title:
+    "Asynchronous JavaScript & EVENT LOOP from scratch 🔥 | Something else that makes too much sense and is definitely not clickbait",
+  channel: "Josh Never Tried Coding",
+  uploaded: "2023-12-03T05:14:34Z",
+};
 
 function SmallVideoCard() {
-  return <div>SmallVideoCard</div>;
+  return (
+    <div className="small_video_card video_card">
+      <div className="video_tumbnail">
+        <Image src={videoDetails.thumbnail} alt="alt" sizes="100%" fill />
+        <span className="ago">{relativeTime(videoDetails.uploaded)}</span>
+      </div>
+      <p className="video_title">{videoDetails.title}</p>
+      <p className="video_channel">{videoDetails.channel}</p>
+      <div className="video_upload">
+        <span className="time">{formatTime(videoDetails.uploaded)}</span>
+        <span>•</span>
+        <span className="date">{formatDate(videoDetails.uploaded)}</span>
+      </div>
+    </div>
+  );
 }
 
 export default SmallVideoCard;
