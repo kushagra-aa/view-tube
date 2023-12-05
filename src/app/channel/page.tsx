710ed5482dc4a3a792b55f9640e773f7af8891c3
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-12-05 14:45:27 +0530

    added channel stats page

--- src/app/channel/page.tsx
+++ src/app/channel/page.tsx
@@ -1,9 +1,78 @@
 "use client";
+import { ChannelType } from "@/models/ChannelType";
 import styles from "./page.module.css";
+import { makeAPIRequest } from "@/lib/useAPI";
+import { useEffect, useState } from "react";
+import useURLSearchParams from "@/hooks/useURLSearchParams";
+import { usePathname } from "next/navigation";
+import { formatDate } from "@/helpers/dateTime";
+import {
+  InfoIcon,
+  SubscriberIcon,
+  VideoIcon,
+  ViewIcon,
+} from "@/components/Icons";
+
+type DataType = { items: ChannelType[] };
+
+const getChannelAPI = async (id: string) => {
+  const data: DataType = await makeAPIRequest("/channels", {
+    id: id,
+    part: "snippet,statistics",
+  }).then((resp: unknown) => resp as DataType);
+  return data.items;
+};
 
 /* /channel?c=<channelID> */
 function ChannelStats() {
-  return <div className={styles.main}>Stats</div>;
+  const [channel, setChannel] = useState<ChannelType>();
+  const [searchParams] = useURLSearchParams();
+
+  const get = async (id: string) => {
+    await getChannelAPI(id).then((d) => {
+      setChannel(d[0]);
+    });
+  };
+
+  useEffect(() => {
+    get(searchParams.c);
+  }, [searchParams.c]);
+  if (!channel) return <></>;
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.stat_card}>
+        <SubscriberIcon />
+        <h2 className={styles.stat_title}>Subscribers:</h2>
+        <p className={styles.stat_value}>
+          {channel.statistics.subscriberCount}
+        </p>
+      </div>
+      <div className={styles.stat_card}>
+        <VideoIcon />
+        <h2 className={styles.stat_title}>Total Videos:</h2>
+        <p className={styles.stat_value}>{channel.statistics.videoCount}</p>
+      </div>
+      <div className={styles.stat_card}>
+        <ViewIcon />
+        <h2 className={styles.stat_title}>Total Views:</h2>
+        <p className={styles.stat_value}>{channel.statistics.viewCount}</p>
+      </div>
+      <div className={styles.stat_card}>
+        <InfoIcon />
+        <h2 className={styles.stat_title}>Joined On:</h2>
+        <p className={styles.stat_value}>
+          {formatDate(channel.snippet.publishedAt)}
+        </p>
+      </div>
+      <div className={styles.description}>
+        <h2 className={styles.description_title}>Description:</h2>
+        <p className={styles.description_value}>
+          {channel.snippet.description}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ChannelStats;
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -278,3 +278,62 @@ export function CommentIcon() {
     </svg>
   );
 }
+export function VideoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+      />
+    </svg>
+  );
+}
+export function SubscriberIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+      />
+    </svg>
+  );
+}
+export function InfoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+  );
+}
